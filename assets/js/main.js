9549629bcbfbf6d7b860cba089d014d21c0b94e3
Cassie <48208000+CassieBeryl@users.noreply.github.com>
2019-10-28 17:44:22 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('#nav-menu').click(function(){
+
+			if( ( $('ul.nav-list').hasClass('open') ) ) {
+
+				$('ul.nav-list').removeClass('open');
+
+			}else{
+
+				$('ul.nav-list').addClass('open');
+
+			}
+
+    });
+});
